45f092e671f4b69864c8b716d8f42ef5f834decb
Luiz Felipe <136415452+LuizMoraisz@users.noreply.github.com>
2025-08-16 01:10:22 -0300

    Add files via upload

---
+++ CursoJS/aula15/index.js
@@ -0,0 +1,19 @@
+console.log(!!(100 / 0));
+
+let num1 = 123;
+console.log(num1.toFixed(2));
+
+//console.log(num1 ** 0.5); // Raiz Quadrada
+
+//console.log(num1 ** (1 / 2)); // Raiz Quadrada
+
+//let num2 = Math.floor(num1); // Arredondar pra baixo
+//let num2 = Math.ceil(num1); // Arredondar pra cima
+//let num2 = Math.round(num1); // Arrendondar de forma automatica
+
+//console.log(Math.max(1, 2, 3, 1555, 123123, 8767, 7, 22)); // Pega o maior numero de uma lista de numeros
+//console.log(Math.min(1, 2, 3, 1555, 123123, 8767, 7, 22)); // Pega o menor numero de uma lista de numeros
+
+//const aleatoria = Math.round(Math.random() * (10 - 5) + 5); // Gerar numeros aleatorios entre 0 e 1 só que o "1" nunca é incluido
+//console.log(Math.round(Math.PI));
+//console.log(Math.pow(2, 10));1
---
+++ CursoJS/tests/texts.js
@@ -0,0 +1,5 @@
+console.log("Joao nasceu em 1984");
+console.log("Em 2000 Joao conheceu Maria");
+console.log("Joao casou-se com maria em 2012");
+console.log("Maria teve 1 filho com joao em 2015");
+console.log("O filho do joao se chama eduardo");
