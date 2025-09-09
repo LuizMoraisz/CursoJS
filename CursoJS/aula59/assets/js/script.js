a92cef17b25de8c362b4e29d703dc2a10899a169
Luiz Felipe <136415452+LuizMoraisz@users.noreply.github.com>
2025-09-09 08:00:42 -0300

    Arrays Avançados
    Revisando sobre Arrays, aprendendo sobre o método splice, filter e map

---
+++ CursoJS/aula59/assets/js/script.js
@@ -0,0 +1,48 @@
+function Calculadora() {
+  this.display = document.querySelector(".display");
+
+  this.inicia = () => {
+    this.capturaCliques();
+    this.capturaEnter();
+  };
+  this.capturaEnter = () => {
+    this.display.addEventListener("keypress", (e) => {
+      if (e.keycode !== 13) {
+        this.realizaConta();
+      }
+    });
+  };
+
+  this.capturaCliques = () => {
+    document.addEventListener("click", (event) => {
+      const el = event.target;
+      if (el.classList.contains("btn-num")) this.addNumDisplay(el);
+      if (el.classList.contains("btn-clear")) this.clear();
+      if (el.classList.contains("btn-del")) this.del(el);
+      if (el.classList.contains("btn-eq")) this.realizaConta(el);
+    });
+  };
+
+  this.realizaConta = (el) => {
+    try {
+      const conta = eval(this.display.value);
+      if (!conta) {
+        alert("Conta Inválida");
+        return;
+      }
+      this.display.value = conta;
+    } catch (e) {
+      alert("Conta Inválida");
+      return;
+    }
+  };
+
+  this.addNumDisplay = (el) => {
+    this.display.value += el.innerText;
+    this.display.focus();
+  };
+  this.clear = () => (this.display.value = "");
+  this.del = (el) => (this.display.value = this.display.value.slice(0, -1));
+}
+const calculadora = new Calculadora();
+calculadora.inicia();
---
+++ CursoJS/aula63/index.js
@@ -0,0 +1,12 @@
+const nomes = ["Maria", "Joao", "Eduardo", "Luiz", "Julia"];
+
+// Push
+nomes.splice(0, 0, "Luiz", "Otavio");
+console.log(nomes);
+
+// nomes.splice(indice, delete, elem1, elem2, elem3)
+// pop
+// const removidos = nomes.splice(-1, 1);
+
+// shift
+// const removidos = nomes.splice(0,1)
