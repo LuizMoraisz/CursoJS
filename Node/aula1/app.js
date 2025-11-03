27c4a85cb74abca4bba2f4f41a5aa75fb83f04c7
luiz felipe <lf860482@gmail.com>
2025-11-03 06:22:36 -0300

    Limpeza da pasta Node para recomeçar do zero

--- Node/aula1/app.js
+++
@@ -1,9 +0,0 @@
-const path = require("path");
-const axios = require("axios");
-const { Pessoa } = require("./mod1");
-const mod1 = require("./mod1");
-
-const p1 = new Pessoa("Joao");
-console.log(mod1);
-
-console.log(p1);
