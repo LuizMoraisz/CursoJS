00f73a134506d485a7e82fb9d5527913278c76b2
Luiz Felipe <lf860482@gmail.com>
2026-04-27 01:59:31 -0300

    Adicionando relacionamento entre as tabelas de aluno e fotos

---
+++ api_rest/src/database/migrations/20260427035625-criar-tabela-de-fotos-do-aluno.js
@@ -0,0 +1,42 @@
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    return queryInterface.createTable("fotos", {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      originalname: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      filename: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      aluno_id: {
+        type: Sequelize.INTEGER,
+        allowNNull: true,
+        references: {
+          model: "alunos",
+          key: "id",
+        },
+        onDelete: "SET NULL",
+        onUpdate: "CASCADE",
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+
+  async down(queryInterface) {
+    return queryInterface.dropTable("fotos");
+  },
+};
